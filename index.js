8e8f60a20f3144b1718d92d5a4434d4b4ed6031c
ForFunPenguin <59682264+ForFunPenguin@users.noreply.github.com>
2022-11-28 20:46:38 +0800

    修正間距

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits, ActivityType, Collection  } = require('discord.js');
+const { Client, GatewayIntentBits, ActivityType, Collection } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 const { token } = require('./config.json');
 const fs = require("fs");
@@ -8,14 +8,14 @@ client.commands = new Collection();
 
 ["command"].forEach(handler => {
   require(`./handler/${handler}`)(client);
- });
+});
 
 client.on('ready', () => {
   console.log(`Hi, ${client.user.username} is now online!`);
 
   client.user.setPresence({
-    activities: [{ name: `正在更新至 v14.6.0`, type: ActivityType.Playing }],
-    status: 'dnd',
+    activities: [{ name: `/help | 頻道指令`, type: ActivityType.Playing }],
+    status: 'online',
   });
 });
 
@@ -24,24 +24,24 @@ client.on('interactionCreate', async interaction => {
 
   const command = interaction.client.commands.get(interaction.commandName);
 
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
 
-  if(client.user.presence.status === "dnd") {
+  if (client.user.presence.status === "dnd") {
     if (!interaction.member.roles.cache.find(x => x.name === "▬▬ 👑 Owner ▬▬⠀") && !interaction.member.roles.cache.find(x => x.name === "▬▬ 🏹 Admin ▬▬⠀")) {
       interaction.reply({ content: '機器人目前處於更新狀態, 指令目前無法使用!', ephemeral: true });
       return;
     }
-  } 
+  }
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: '執行指令時發生未知錯誤!', ephemeral: true });
-	}
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({ content: '執行指令時發生未知錯誤!', ephemeral: true });
+  }
 });
 
 client.login(token);
