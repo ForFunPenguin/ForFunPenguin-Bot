b41aa797b091bd92a11079c09dc8dbf4a963ece2
ForFunPenguin <raymondlu89@gmail.com>
2023-02-03 04:23:27 +0800

    V3.0.2 fix link

--- commands/modpacks/modpacks.js
+++ commands/modpacks/modpacks.js
@@ -16,9 +16,9 @@ module.exports = {
     async execute(interaction) {
         const packName = interaction.options.getString('模組包名稱')
         if (packName == "Skyblock") {
-            interaction.reply({content: `${interaction.member} \nhttps://drive.google.com/file/d/1ABdUrveLU1g__3d3qRo6d4vx3WtPIZ7R/view?usp=sharing`, ephemeral: true});
+            interaction.reply({content: `${interaction.member} \nhttps://drive.google.com/file/d/1szXO0TveY43rdQJ2sHceBNN2XiaInjI7/view?usp=share_link`, ephemeral: true});
         } else if (packName == "Wynncraft") {
-            interaction.reply({content: `${interaction.member} \nhttps://drive.google.com/file/d/1oODYAD2ZIPNgc9ZB6rwBIZG0VaG1Tlte/view?usp=sharing`, ephemeral: true})
+            interaction.reply({content: `${interaction.member} \nhttps://drive.google.com/file/d/1QeRCF1MCTIo5UE5-OtZDgBeUuhIPQMGD/view?usp=share_link`, ephemeral: true})
         } else {
             interaction.reply({content: `無法找到該模組包!`, ephemeral: true});
         }
